bf85c3b871d1c1b42f6e637b3911de349a23e387
Loic <leotetloic08@gmail.com>
2026-05-08 13:34:35 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import banner from"../assets/images/banner.jpg";
+import { NavLink} from"react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Header() {
+    return (
+        <header className="navbar navbar-expand-lg bg-light-shadow-sm">
+            <div className="container">
+                <NavLink className="navbar-brand d-flex align-items-center" to="/">
+                <img src={banner} alt="Bannière" height="40" className="me-2" />
+                        JOHN DOE
+                </NavLink>
+
+                
+
+            </div>
+
+        </header>
+    )
+}
