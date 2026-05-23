a1978df09db4da4d88658fef9d865de1891114b6
Loic <leotetloic08@gmail.com>
2026-05-23 18:21:37 +0200

    Mise à jour de la page MentionsLegales.jsx

--- src/pages/MentionsLegales.jsx
+++ src/pages/MentionsLegales.jsx
@@ -8,12 +8,53 @@ export default function MentionsLegales() {
                 <title>Mentions Légales</title>
             </Helmet>
 
-            <h1>Mentions Légales</h1>
-            
-            <div className="accordeon">
-                
-                <p>Contenu des mentions légales...</p>
+            <h2 className="text-center mb-4">Mentions Légales</h2>
+
+            <div className="accordion" id="accordionMentions">
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseIdentite" aria-expanded="true" aria-controls="collapseIdentite">
+                            Editeur du site
+                        </button>
+                    </h2>
+                    <div id="collapseIdentite" className="accordion-collapse collapse show">
+                        <div className="accordion-body">
+                            John Doe - Développeur web.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseHébergement">
+                            Hébergeur 
+                        </button>
+                    </h2>
+                    <div id="Hébergement" className="accordion-collapse collapse">
+                        <div className="accordion-body">
+                            Hébergement par : Nom de l'hébergeur.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#credits">
+                            Credits 
+                        </button>
+                    </h2>
+                    <div id="credits" className="accordion-collapse collapse">
+                        <div className="accordion-body">
+                            images: <a href="https://www.pixabay.com/"  target="_blank" rel="noopener noreferrer" title="Pixabay">Pixabay</a> 
+                            Icônes: <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer" title="Flaticon">Flaticon</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </> 
     );
+
 }
+            
+
