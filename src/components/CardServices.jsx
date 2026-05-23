7c202669aa56a3a9713b39d1b58303a0a14577f7
Loic <leotetloic08@gmail.com>
2026-05-23 19:38:47 +0200

    Mise à jour de CardServices.jsx

--- src/components/CardServices.jsx
+++ src/components/CardServices.jsx
@@ -1,4 +1,4 @@
-export default function CardService({ icon, titre, texte }) {
+export default function CardServices({ icon, titre, texte }) {
     return (
         <div className="col-md-4 mb-4">
             <div className="card card-hover shadow-sm p-3 h-100 text-center">
