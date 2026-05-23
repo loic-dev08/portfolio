2d62536efa432f57c22bbc9b4087d5e78e835ad1
Loic <leotetloic08@gmail.com>
2026-05-23 18:04:45 +0200

    Ajout du fichier Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,48 @@
+export default function Contact() {
+  return (
+    <section className="container py-5">
+        <h2 className="text-center mb-4">Contactez-moi</h2>
+
+        <div className="row">
+            <div className="col-md-6">
+                <form className="cp-4 shadow-sm bg-light rounded">
+
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Nom</label>
+                        <input type="text" className="form-control" id="name" name="name" required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" name="email" required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="message" className="form-label">Message</label>
+                        <textarea className="form-control" id="message" name="message" rows="5" required></textarea>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary w-100">Envoyer</button>
+                </form>
+            </div>
+
+            <div className="col-md-6">
+                <h4 className="mb-4-md-0">Coordonnées</h4>
+                <p>10 Rue Exemple, 75000 Paris</p>
+                <p>Téléphone: 01 23 45 67 89</p>
+                <p>Email: john.doe@example.com</p>
+
+                <iframe
+                title="Google Maps"
+                src="https://www.google.com/maps/place/Carnon,+Mauguio/@43.5455664,3.9751996,16z/data=!3m1!4b1!4m6!3m5!1s0x12b6bb4323d2abcd:0x2f1cdecb734b927b!8m2!3d43.5488256!4d3.9799375!16s%2Fg%2F120wktg5?authuser=0&entry=ttu&g_ep=EgoyMDI2MDUxMS4wIKXMDSoASAFQAw%3D%3D"
+                width="100%"
+                height="350"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                ></iframe>
+            </div>
+        </div>
+    </section>
+  );
+}
